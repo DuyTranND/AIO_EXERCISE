b9f4d39fbd7de821bf023a833ddc9b650d078047
DuyTranND <duytranus5300@gmail.com>
2024-06-17 18:27:04 +0700

    Add Ex3_4 Word Counting

--- word_counting.py
+++ word_counting.py
@@ -1,9 +1,17 @@
 # Word Counting
-def main():
-    pass
+def word_count(file_path):
+    f = open(file_path)
+    f = f.read()
+    lis = f.lower().split()
+    unit = set([str(elem) for elem in lis])
+    dic = {}
+    for i in unit:
+        lis.count(i)
+        dic[i] = lis.count(i)
+    return dic
 
 
 file_path = "F:\AIO\AIO_Homework\Module1\Week2\Exercise\P1_data.txt"
 
 if __name__ == "__main__":
-    main()
+    print(word_count(file_path))
